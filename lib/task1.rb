215cd5714360e8ac6ea61f8c9e0353902a5c079d
Harrisam <samantha.s.harris@googlemail.com>
2013-09-13 18:52:48 +0100

    corretion of task 1

--- lib/task1.rb
+++ lib/task1.rb
@@ -1,13 +1,9 @@
 class Array
 
-	def inject_sum(array)
-		return 0 if array.empty?
-		array[0] + inject_sum(array[1..-1])
-	end
-
-	def inject_product(array)
-		final =1.00
-		array.each { |i| final *= i}
-		final
+	def inject_s
+		accum = self.shift
+		self.each { |value| accum = yield(accum, value)}
+		accum
 	end
 end
+
--- spec/task1_spec.rb
+++ spec/task1_spec.rb
@@ -2,18 +2,11 @@ require 'task1'
 # renamed inject because rspec uses it to run and caused issues.
 describe 'Array' do
 
-	let(:array) {Array.new}
-
-	it 'should compute the sum of an empty array' do
-		expect(array.inject_sum([])).to eq 0
+	it 'should produce a sum of an array' do
+		expect([1,2,3,4].inject_s {|accum, value| accum + value}).to eq 10
 	end
 
-	it 'should sum all the elements in an array' do  
-		expect(array.inject_sum([1,2,3,4,5])).to eq 15
+	it 'should produce an interpolated string' do  
+		expect(["1", "2", "3", "4"].inject_s {|accum, value| accum + value}).to eq "1234"
 	end
-
-	it 'should return the product of the elements in an array' do  
-		expect(array.inject_product([1,2,3,4,5])).to eq 120.00
-	end
-
 end
